ab67f5f91fad1c7ac12b5201ed26b583a1a9c586
patdy074 <149289275+patdy074@users.noreply.github.com>
2025-04-20 13:40:21 +0930

    Initial Project setup with all the folders and files inculded as per the assignment specification and Added provided starter code like Game_Title file and Added System design document and UML diagram

---
+++ DeadMansDraw++/DeadMansDraw++.cpp
@@ -0,0 +1,12 @@
+﻿// DeadMansDraw++.cpp : Defines the entry point for the application.
+//
+
+#include "DeadMansDraw++.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Game_Title.h"
+
+int main() {
+
+    std::cout << GAME_TITLE << std::endl;
+    return 0;
+}
